8e1864ded75110376d839714e4606444816864cd
Carl <carl_hansen@hotmail.com>
2015-01-10 17:01:34 +1000

    Added some textures and changed configurations

--- desktop/src/com/carlipoot/swivelstick/desktop/DesktopLauncher.java
+++ desktop/src/com/carlipoot/swivelstick/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.carlipoot.swivelstick.SwivelStick;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Swivel Stick";
+		config.width = 480;
+		config.height = 800;
+
 		new LwjglApplication(new SwivelStick(), config);
 	}
 }
